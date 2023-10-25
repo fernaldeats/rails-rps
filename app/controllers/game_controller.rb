567e783f38df6b5fa510b458bae8f282caef73d2
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-10-25 14:41:01 +0000

    more or less complete pending test

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,54 @@
+class GameController < ApplicationController
+  def user_home
+    render({ :template => "game_template/home"})
+  end
+  
+  def user_played_rock
+    moves = ["rock", "paper", "scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "scissors"
+      @outcome = "won"
+    else
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_template/play_rock"})
+  end
+
+  def user_played_scissors
+    moves = ["rock", "paper", "scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "scissors"
+      @outcome = "tied"
+    else
+      @outcome = "won"
+    end
+
+    render({ :template => "game_template/play_scissors"})
+  end
+
+  def user_played_paper
+    moves = ["rock", "paper", "scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_template/play_paper"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "game", :action => "user_home"})
+  get("/rock", { :controller => "game", :action => "user_played_rock" })
+  get("/scissors", { :controller => "game", :action => "user_played_scissors" })
+  get("/paper", { :controller => "game", :action => "user_played_paper" })
 end
